dd21e4a47b9a9e5351b970ddaf2b8b4473341062
Javier Ignacio Tibi <jtibi@HEXACTA.COM>
2018-05-16 12:01:31 -0300

    rm folders

--- CordovaPluginVrViewSampleApp/src/models/media-sample.model.ts
+++
@@ -1,9 +0,0 @@
-export class MediaSampleModel {
-    name: string;
-    type: string;
-    inputType: string;
-    inputFormat: string;
-    url: string;
-    isLocal: boolean;
-    previewUrl: string;
-}
